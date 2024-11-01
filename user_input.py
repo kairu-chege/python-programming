cb84cadc5a3907d93b69f2230a0a9d28474cb245
kairu-chege <danielkairuchege@gmail.com>
2024-11-01 10:15:10 +0300

    User input assignment for week one

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("what is your name?")
+age = input("How old are you?")
+location = input("Where do you live?")
+
+print(f"Hello {name}! You are {age} years old and live in {location}. Its a plasure to meet you.")
